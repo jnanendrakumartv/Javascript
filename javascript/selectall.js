db8ec4935e72e20684e40096909fac59c7d26ab2
admin <jvijayakumarostb1@gmail.com>
2019-05-29 14:41:11 +0530

    checkbox

--- javascript/selectall.js
+++ javascript/selectall.js
@@ -1,4 +1,5 @@
 			function SelecteAll(){
+				debugger;
 				var items=document.getElementsByName('sel');
 				for(var i=0; i<items.length; i++){
 					if(items[i].type=='checkbox')
@@ -14,12 +15,13 @@
 				}
 			}	
            function calculate(){
+           	debugger;
         var total=0;
         var items=document.getElementsByName("sel");
         for(var i=0; i<items.length; i++){
         	if(items[i].type=="checkbox" && items[i].checked==true)
         		total=parseInt(items[i].value)+total;
            }   
-          document.getElementById("output").innerHTML="The total is: "+total;
+          document.getElementById("output").innerHTML="Total is: "+total;
                 }
 
